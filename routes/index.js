30bda33303bd790b059cdc00575c12c42ffd1d41
Sarah Abing <abingsa1@gmail.com>
2019-04-02 19:36:29 -0600

    webpage populates with extra life data from VLK - fixed styling - imported requests for the initial request of getting the team

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,28 @@ This might be needed for the node extra life API module
 
  */
 import express from 'express';
-import { getTeamInfo, getTeamDonations } from 'extra-life-api';
-
+import request from 'request';
+import { getTeamDonations, getTeamRoster } from 'extra-life-api';
 
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.render('index');
+    request("https://extra-life.donordrive.com/api/teams/44021", function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+            getTeamDonations(44021, 50).then((donations) => {
+        console.log(donations);
+                getTeamRoster(44021).then(roster => {
+                    res.render('index', {body: JSON.parse(body), donations: donations.donations, roster: roster.members});
+                })
+            })
+    .catch((e) => {
+        console.log(e);
+    });
+        }
+    })
+
 });
 
 
